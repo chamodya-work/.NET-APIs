e62c76925f6aee2d20e245df862a4af6e2ce2d6e
chamodya <chamodyarajapaksha1@gmail.com>
2025-08-21 11:10:41 +0530

    testing Web Api end poinds

--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var todos = new List<Todo>();
+app.MapGet("/todos", () =>todos);
+app.MapGet("/", () => "Welcome to the Todo API!");
+// GET endpoint
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => t.Id == id);
+    return targetTodo is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetTodo);
+});
 
+// POST endpoint
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created($"/todos/{task.Id}", task);
+});
+
+app.MapDelete("/todos/{id}", (int id) =>
+    {
+    todos.RemoveAll(t=> t.Id == id);
+    return TypedResults.NoContent();
+});
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsComplete);
